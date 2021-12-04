2ea733d0bce6ae9f4a2516b905e1f17cecfe2be3
Jennifer Carrera <jennifer.s.carrera@gmail.com>
2021-12-04 16:09:26 -0500

    Update AestheticsActivity.java

--- app/src/main/java/CDFFlint/pilot/cdf_multiscreen/AestheticsActivity.java
+++ app/src/main/java/CDFFlint/pilot/cdf_multiscreen/AestheticsActivity.java
@@ -3,6 +3,13 @@ package CDFFlint.pilot.cdf_multiscreen;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.util.Log;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 public class AestheticsActivity extends AppCompatActivity {
 
@@ -10,5 +17,57 @@ public class AestheticsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_aesthetics);
+    }
+
+    /**
+     * This method is called when the submit button is clicked.
+     */
+    public void submitAestheticsResults(View view) {
+
+        // Get appearance of water
+        EditText appearanceField = (EditText) findViewById(R.id.water_appearance_description);
+        Editable appearanceEditable = appearanceField.getText();
+        String appearance = appearanceEditable.toString();
+
+        // Get smell of water
+        EditText smellField = (EditText) findViewById(R.id.water_smell_description);
+        Editable smellEditable = smellField.getText();
+        String smell = smellEditable.toString();
+
+        // Get taste of water
+        EditText tasteField = (EditText) findViewById(R.id.water_taste_description);
+        Editable tasteEditable = tasteField.getText();
+        String taste = tasteEditable.toString();
+
+        // Display the results summary
+        String message = createResultsSummary(appearance, smell, taste);
+
+        String subject = getString(R.string.email_subject);
+        String email_address = getString(R.string.email_address);
+
+        display(message);
+    }
+
+    /**
+     * Create summary of the results.
+     * @return text summary
+     */
+    private String createResultsSummary(String appearance, String smell,String taste) {
+
+        String summaryMessage = "\n " + getString(R.string.appearance_short) + ": " + appearance + "\n";
+        summaryMessage += "\n " + getString(R.string.smell_short) + ": " + smell + "\n";
+        summaryMessage += "\n " + getString(R.string.taste_short) + ": " + taste;
+
+        return summaryMessage;
+    }
+
+    /**
+     * This method displays the summary to the screen.
+     */
+    private void display(String summary) {
+        TextView summaryTextView = (TextView) findViewById(R.id.summary);
+        summaryTextView.setText("Summary: \n" + summary);
+        summaryTextView.setVisibility(View.VISIBLE);
+
     }
 }
